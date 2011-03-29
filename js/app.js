f3b1fd717a49f46325e5316056373f575eae30b9
rwldrn <waldron.rick@gmail.com>
2011-03-29 17:49:29 -0400

    Adds a lot of code

--- js/app.js
+++ js/app.js
@@ -1,175 +1,190 @@
+/*
+ * Rotoscoper app.js
+ * https://github.com/boazsender/Rotoscoper
+ *
+ * Copyright (c) 2010 Boaz Sender
+ * Authors: Boaz Sender
+ * Dual licensed under the MIT and GPL licenses.
+ * http://code.bocoup.com/license/
+ * 
+ * Canvas drawing code based on work from Mike Taylor's home page
+ * "_MT.CanvasDrawr" at http://miketaylr.com
+ *
+ */
 
 (function( global, $ ) {
 
-  //  Remote load underscore
-  var head = document.head || document.getElementsByTagName("head")[0] || document.documentElement, 
-      script = document.createElement("script");
 
-  script.async = true;
-  script.src = "http://documentcloud.github.com/underscore/underscore-min.js";
-
-  //  When underscore is available, define our ctor
-  script.onload = function() {
-
-    //  rotoFrame public API
-    function rotoFrame( options ) {
-      return new Frame( options );
-    }
-
-    //  Frame ctor
-    //    deps: Popcorn, jQuery, Underscore
-    function Frame( options ) {
-
-      var self = this;
-
-      options.timeout || ( options.timeout = 0 );
-    
-      _.extend( this, options );
-
-
-      this.kernal.listen( "play", function() {
-
-        //  Call the rendering throttler
-        self.throttle();
-
-      }); 
-
-      this.initMouse();
-    
-      return this;
-    }
-
-
-    Frame.prototype.initMouse = function() {
-      var self = this;
-      //set pX and pY from first click
-      this.canvas
-        .bind('mousedown', function( e ) {
-
-          self.setAnchorPoint( e );
-
-        })
-        .bind('mouseup', function( e ){
-          self.canvas
-            .unbind('mousemove')
-        })
-    };
-
-    Frame.prototype.setAnchorPoint = function( e ) {
-      var self = this;
-      
-      this.canvas.bind('mousemove', function( e ) {
-        self.draw( e );
-      });
-      
-      this.context.pX = e.pageX;
-      this.context.pY = e.pageY;
-
-      e.preventDefault();
-    };
-
-    Frame.prototype.draw = function( e ) {
-      var moveX = e.pageX - this.context.pX,
-          moveY = e.pageY - this.context.pY;
-
-      this.move( moveX, moveY );
-    };
-    
-    Frame.prototype.move = function(changeX, changeY) {
-      this.context.beginPath();
-      this.context.moveTo( this.context.pX,this.context.pY );
-
-      this.context.pX += changeX;
-      this.context.pY += changeY;
-
-      this.context.lineTo( this.context.pX, this.context.pY );
-      this.context.stroke();
-    };
-    
-    Frame.prototype.storage  = function( item ) {
-      if ( !localStorage.getItem( 'Rotoscoper' ) ){
-        localStorage.setItem( 'Rotoscoper', '{}' );
-      }
-      
-      if ( item ) {
-        return JSON.parse( localStorage.getItem( 'Rotoscoper' ) )[ item ];
-      }
-
-      return JSON.parse( localStorage.getItem( 'Rotoscoper' ) );
-    };
-    
-    Frame.prototype.saveFrameData = function() {
-
-      var _storage = this.storage();
-      
-      _storage[ this.kernal.currentTime().toFixed(1) + ''] = this.context.canvas.toDataURL();
-      
-      localStorage.setItem('Rotoscoper', JSON.stringify( _storage ));
-
-      this.context.clearRect(0, 0, 600, 335)
-    };    
-
-    Frame.prototype.throttle = function() {
-      //  Return immediately if paused/ended
-      if ( this.kernal.media.paused || this.kernal.media.ended ) {
-        return;
-      }
-      
-      //  Process the current scene
-      this.render();
-      
-      //  Store ref to `this` context
-      var self = this;
-      
-      //  The actual throttling is handled here, 
-      //  throttle set to 20 fps
-      setTimeout(function () {
-        
-        //  Recall the processing throttler
-        self.throttle();
-
-      }, this.timeout );  
-    }
-
-    Frame.prototype.render = function() {
-
-      $('#time').html( this.kernal.currentTime().toFixed(1) );
-
-      this.context.clearRect(0, 0, 600, 335);
-
-      var frame, _img;
-
-      // ^ RW: this is a VERY cool trick - what if you loaded all the "scenes" 
-      //        in earlier and hide them in the DOM?
-      frame = this.storage( this.kernal.currentTime().toFixed( 1 ) );
-
-      //  Return if no frame to draw
-      if ( !frame ) {
-        return;
-      }
-
-      _img = $('<img>', {
-        src : frame
-      }).get(0)
-
-      this.context.drawImage( _img, 0, 0, 600, 335 );
-
-      // console.log( app.storage( this.currentTime().toFixed(1) ) );  
-    }
-
-
-    global.rotoFrame = rotoFrame;  
+  //  rotoFrame public API
+  function rotoFrame( options ) {
+    return new Frame( options );
   }
 
-  head.insertBefore( script, head.firstChild );
+  //  Frame ctor
+  //    deps: Popcorn, jQuery, Underscore
+  function Frame( options ) {
 
+    var self = this;
 
+    options.fps || ( options.fps = 35 );
+  
+    _.extend( this, options );
 
+    this.frames = {
+      data: null, 
+      last: null
+    };
+    
+    this.kernal.listen( "play", function() {
+
+      self.frames.data = self.storage();
+
+      //  Call the rendering throttler
+      self.throttle();
+
+    }); 
+
+    this.initMouse();
+  
+    return this;
+  }
+
+  Frame.prototype.initMouse = function() {
+    var self = this;
+    //set pX and pY from first click
+    this.canvas
+      .bind('mousedown', function( e ) {
+
+        self.setAnchorPoint( e );
+
+      })
+      .bind('mouseup', function( e ){
+        self.canvas
+          .unbind('mousemove')
+      })
+  };
+
+  Frame.prototype.setAnchorPoint = function( e ) {
+    var self = this;
+    
+    this.canvas.bind('mousemove', function( e ) {
+      self.draw( e );
+    });
+    
+    this.context.pX = e.pageX;
+    this.context.pY = e.pageY;
+
+    e.preventDefault();
+  };
+
+  Frame.prototype.draw = function( e ) {
+    var moveX = e.pageX - this.context.pX,
+        moveY = e.pageY - this.context.pY;
+
+    this.move( moveX, moveY );
+  };
+  
+  Frame.prototype.move = function(changeX, changeY) {
+    this.context.beginPath();
+    this.context.moveTo( this.context.pX,this.context.pY );
+
+    this.context.pX += changeX;
+    this.context.pY += changeY;
+
+    this.context.lineTo( this.context.pX, this.context.pY );
+    this.context.stroke();
+  };
+  
+  Frame.prototype.storage  = function( item ) {
+    if ( !localStorage.getItem( 'Rotoscoper' ) ){
+      localStorage.setItem( 'Rotoscoper', '{}' );
+    }
+    
+    if ( item ) {
+      return JSON.parse( localStorage.getItem( 'Rotoscoper' ) )[ item ];
+    }
+
+    return JSON.parse( localStorage.getItem( 'Rotoscoper' ) );
+  };
+  
+  Frame.prototype.saveFrameData = function() {
+
+    var _storage = this.storage();
+
+    _storage[ this.kernal.currentTime().toFixed(1) + ''] = this.context.canvas.toDataURL();
+    
+    localStorage.setItem('Rotoscoper', JSON.stringify( _storage ));
+
+    this.context.clearRect(0, 0, 600, 335)
+  };    
+
+  Frame.prototype.throttle = function( time ) {
+    //  Return immediately if paused/ended
+    if ( this.kernal.media.paused || this.kernal.media.ended ) {
+      return;
+    }
+
+    time = +( ( time > 0 && +time.toFixed( 1 ) ) || time );
+    //  Process the current scene
+
+    this.render( time );
+    
+    //  Store ref to `this` context
+    var self = this;
+    
+    //  The actual throttling is handled here, 
+    //  throttle set to 20 fps
+    setTimeout(function () {
+      
+      //  Recall the processing throttler
+      self.throttle( time + .1 );
+
+    }, 1000/this.fps );  
+  }
+
+  Frame.prototype.render = function( time ) {
+
+    time = this.kernal.currentTime().toFixed( 1 );
+
+    //  Update time display 
+    if ( this.hasTimeDisplay ) {
+      $('#time').html( time );
+    }
+    
+    //  Clear last frame
+    this.context.clearRect( 0, 0, 600, 335 );      
+
+    var frame, $img;
+
+    //  Get frame data from cached frames index
+    frame = this.frames.data[ time ];
+
+    //  Return if no frame to draw
+    if ( !frame ) {
+
+      frame = this.frames.last;
+      
+      return;
+    }
+
+    
+    $img = $('<img>', {
+      src : frame
+    }).get(0);
+
+    this.frames.last = frame;
+    
+    this.context.drawImage( $img, 0, 0, 600, 335 );
+
+  };
+
+  global.rotoFrame = rotoFrame;  
 
 })( window, jQuery );
 
 $(function(){
   var video = $('<video>', {
+        id: 'subject',
         src: 'trailer.ogv',
         css:{
           width: 600,
@@ -186,7 +201,7 @@ $(function(){
 
 
 
-  kernal.listen('canplaythrough', function(){
+  kernal.listen('canplaythrough', function() {
 
     var canvas = $('#canvas'),
         context = canvas.get(0).getContext("2d");
@@ -197,24 +212,68 @@ $(function(){
     context.pX = undefined;
     context.pY = undefined;
 
+    
+
     var roto = rotoFrame({
                   kernal: kernal,
                   context: context, 
-                  canvas: canvas
+                  canvas: canvas, 
+                  hasTimeDisplay: true
                 });
-    
-    $('#advance').click(function(){
-      kernal.currentTime(kernal.currentTime() + .1);
-      roto.saveFrameData();
+
+
+    $('input[name="colorpicker"]').bind('change blur', function() {
+
+      var val = $(this).val();
+
+      if ( context && context.strokeStyle ) {
+        context.strokeStyle = val;
+      }
     });
+
+    $('input[name="sizepicker"]').bind('change', function() {
+
+      var val = $(this).val();
+
+      if ( context && context.lineWidth ) {
+        context.lineWidth = val;
+      }
+    
+    });
+
+    $('#advance-fw,#advance-rw').click(function(){
+
+      roto.saveFrameData();
+
+      kernal.currentTime(
+        $(this)[0].id === 'advance-rw' ?
+          kernal.currentTime() - .1 :
+          kernal.currentTime() + .1
+      );
+
+      $('#time').html( kernal.currentTime().toFixed(1) );
+
+    });
+    
+    $('#reset').click(function(){
+      kernal.currentTime(0);
+    });    
 
     $('#playback').toggle(function(){
       kernal.play();
-      $(this).text('Pause');
+      $(this).text('||').attr('alt', 'pause');
     }, function(){
       kernal.pause();
-      $(this).text('Play');
-    })
+      $(this).text('>').attr('alt', 'play');
+    });
+
+    var $footer = $('footer');
+    
+    $('input,button').hover(function() {
+      $footer.html( $(this).attr('alt') );
+    }, function() {
+      $footer.empty();
+    });
 
   });
 })
